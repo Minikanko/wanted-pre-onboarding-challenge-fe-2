c4e25293455461f910b7e2d7c56c9883e09fcd6c
Mr.k <bcgrhio@naver.com>
2023-02-08 22:35:08 +0900

    feat: TypeScript를 활용한 데이터 모델링

--- src/index.ts
+++ src/index.ts
@@ -10,59 +10,56 @@
  * @property {string[]} [tags] - 태그들
  */
 
+interface ITodo {
+  id: number;
+  content: string;
+  isComplete: boolean;
+  category?: string[];
+  tags?: string[];
+}
 /**
  * 내용 없이 추가할 수 없다.
- * @param {string} content
- * @param {string[]?} category
- * @param {string[]?} tags
- * @return {Todo}
- * @throws {IsNullContent}
  */
-function CREATE(content, category, tags) {}
+interface IAddTodo {
+  content: string;
+  category?: string[];
+  tags?: string[];
+}
+
+function create(_fn: ({ content, category, tags }: IAddTodo) => ITodo) {}
 
 /**
  * 모든 할 일을 조회할 수 있다.
  * ID를 기반으로 특정 할 일을 조회할 수 있다.
- * @type {number} id
- * @return {Todo}
  */
-function read(id) {}
+function read(_fn: (id: number) => ITodo) {}
 
 /**
  * ID를 제외한 모든 속성을 수정할 수 있다.
  * 특정 할 일의 특정 태그를 수정할 수 있다.
- * @type {Todo} todo
- * @return {Todo}
  */
-function update(todo) {}
+function update(_fn: (todo: ITodo) => ITodo) {}
 
 /**
  * 모든 할 일을 제거할 수 있다.
- * @return {void}
  */
-function deleteAllTodo() {}
+function deleteAllTodo(): void {}
 
 /**
- * 특정 TODO 삭제
  * ID를 기반으로 특정 할 일을 삭제할 수 있다.
- * @param {string} id
- * @return {void}
  */
-function deleteById(id) {}
+function deleteById(_fn: (id: string) => void) {}
 
 /**
- * 특정 TODO의 특정 태그 삭제
  * 특정 할 일의 특정 태그를 삭제할 수 있다.
- * @param {string} id
- * @param {string[]} targetTags
- * @return {void}
  */
-function deleteTag(id, targetTags) {}
+interface IRemoveTag {
+  id: number;
+  targetTags: string[];
+}
+function deleteTag(_fn: ({ id, targetTags }: IRemoveTag) => ITodo) {}
 
 /**
- * 특정 TODO의 모든 태그 삭제
  * 특정 할 일의 모든 태그를 제거할 수 있다.
- * @param {string} id
- * @return {void}
  */
-function deleteAllTag(id) {}
+function deleteAllTag(_fn: ({ id: number }) => ITodo) {}
